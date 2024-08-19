a5e4577e21c61f700fa7d81a4ee5f103062ae40d
Yason Khaburzaniya <yason@hey.com>
2024-08-19 16:53:16 -0700

    use a poetry script to start cli

---
+++ README.md
@@ -0,0 +1,7 @@
+# Breba Docs
+
+_AI documentation validator_ 
+
+## Features
+Scans your documentation file and executes commands in the documentation
+to make sure that it is possible to follow the documentation.
--- breba_docs/cli.py
+++ breba_docs/cli.py
@@ -3,9 +3,10 @@ import os.path
 import docker
 import requests
 
+from breba_docs.analyzer.service import analyze
 from breba_docs.services.openai_agent import OpenAIAgent
 from dotenv import load_dotenv
-from  urllib.parse import urlparse
+from urllib.parse import urlparse
 
 
 def is_valid_url(url):
@@ -19,38 +20,7 @@ def is_file_path(path):
     return os.path.exists(path)
 
 
-def run(agent, container, doc):
-
-    commands = agent.fetch_commands(doc)
-
-    chained_commands = ' && '.join(commands)
-
-    # Execute a command in the container with real-time output
-    exit_code, output = container.exec_run(
-        f'/bin/bash -c "{chained_commands}"',
-        stdout=True,
-        stderr=True,
-        tty=True,
-        stream=True,
-    )
-
-    output_text = ""
-
-    for line in output:
-        line_text = line.decode("utf-8")
-        print(line_text.strip())
-        output_text += line_text
-
-    print(agent.analyze_output(output_text))
-
-    result = container.exec_run('ls -la')
-    print(result.output.decode('utf-8'))
-
-    container.stop()
-    container.remove()
-
-
-if __name__ == "__main__":
+def run():
     load_dotenv()
     client = docker.from_env()
     started_container = client.containers.run(
@@ -80,6 +50,6 @@ if __name__ == "__main__":
         print(errors)
     elif document:
         ai_agent = OpenAIAgent()
-        run(ai_agent, started_container, document)
+        analyze(ai_agent, started_container, document)
     else:
         print("Document text is empty, but no errors were found")
